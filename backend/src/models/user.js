9293b2366e4c386bb78cf11149526f2483a80ed0
somesh <someshsrichandan@gmail.com>
2024-10-26 21:08:25 +0530

    user model created

---
+++ backend/src/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'First name is required']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required']
+    },
+    emailId: {
+        type: String,
+        required: [true, 'Email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required']
+    },
+    gender: {
+        type: String,
+        required
+    },
+});
+
+const userModel = mongoose.model('User', userSchema);
+module.exports = userModel;
