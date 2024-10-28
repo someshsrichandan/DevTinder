2894c12f456f91c7e556954048577068c37824a4
somesh <someshsrichandan@gmail.com>
2024-10-29 00:22:38 +0530

    validator signup data

--- backend/src/utils/validation.js
+++ backend/src/utils/validation.js
@@ -1,3 +1,17 @@
+const validator = require('validator');
+
 const validateSignUpData = (req) =>{
     const {firstName , lastName , emailId , password} = req.body;
+    if(!firstName || !lastName){
+        throw new Error("First name and last name are required");
+    }
+    else if(!validator.isEmail(emailId)){
+        throw new Error("Emaail is invalid");
+    }else if(!validator.isStrongPassword(password)){
+        throw new Error("Please enter a strong password");
+    }
+}
+
+module.exports = {
+    validateSignUpData
 }
