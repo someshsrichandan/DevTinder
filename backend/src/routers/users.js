2c0effa912d3dda015c9f8f049a832119ce27d25
somesh <someshsrichandan@gmail.com>
2025-01-19 21:21:49 +0530

    feed api done

--- backend/src/routers/users.js
+++ backend/src/routers/users.js
@@ -64,6 +64,10 @@ userRouter.get('/feed',userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
         //user should see all user card expect
         //1. user itself
         //2. user with whom connection is already established
@@ -87,8 +91,10 @@ userRouter.get('/feed',userAuth, async (req, res) => {
             {_id: { $nin: Array.from(hideUserFromFeeds)}},
             {_id: { $ne: loggedInUser._id}}
            ] 
-        }).select(USER_SAFE_DATA);
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
         
+
+        res.json({data: users});
     } catch (error) {
         console.log(error);
         res.status(400).send("Request failed");
