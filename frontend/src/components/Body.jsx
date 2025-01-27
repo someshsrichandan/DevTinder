94231855845af7a55993c8d309bffd8b73262e0f
somesh <someshsrichandan@gmail.com>
2025-01-27 09:05:14 +0530

    feat: fetch user profile on component mount and update Redux store

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,15 +1,40 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "./Navbar"
-import Footer from "./Footer"
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { BASE_URL } from "../utils/constant";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  
+    const dispatch = useDispatch();
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(BASE_URL + "/profile/view", {
+          withCredentials: true,
+        });
+        console.log(res.data);
+        dispatch(addUser(res.data));
+      } catch (error) {
+        console.log(error);
+        
+      }
+      
+    };
+
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
-    <Navbar/>
-    <Outlet/>
-    <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
